af8a0c57be74059c8c908a5f3765ac63c368eacd
Klaus Essig Schweizer <klaus@spmodelismo.com.br>
2025-10-03 19:28:31 -0300

    Banco de dados inicial

--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,1 +1,12 @@
 <?php
+
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=dbshopping',
+    'root',
+    'root',
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]
+);
