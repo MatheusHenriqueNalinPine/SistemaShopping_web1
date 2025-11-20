a915c836d8ab54140d72b921f2d34612734d01e6
Klaus Essig Schweizer <klaus@spmodelismo.com.br>
2025-11-20 18:54:32 -0300

    Cadastrar/excluir/editar categoria de lojas

---
+++ src/model/repositorio/CategoriaLojaRepositorio.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace model\repositorio;
+
+use PDO;
+
+class CategoriaLojaRepositorio
+{
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function salvar(string $categoria)
+    {
+        $sql = "insert into tbcategorialoja (categoria) values (?);";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $categoria);
+        $stmt->execute();
+    }
+
+    public function alterar(int $id, string $categoria)
+    {
+        $sql = "update tbcategorialoja set categoria = ? where id = ?;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $categoria);
+        $stmt->bindValue(2, $id);
+        $stmt->execute();
+    }
+
+
+    public function excluir(int $id): void
+    {
+        $sql = "delete from tbcategorialoja where id = ?;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+    }
+
+
+    public function buscarTodas(): array
+    {
+        $sql = "select * from tbcategorialoja;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function buscarPorId(int $id)
+    {
+        $sql = "select * from tbcategorialoja where id = ?;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    public function isCategoriaUsed(int $id): bool
+    {
+        $sql = "SELECT 1 FROM tbLoja WHERE id_categoria = :id LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        return (bool) $stmt->fetchColumn();
+    }
+
+}
---
+++ src/view/administrativo/loja/categoria/editar-categoria.php
@@ -0,0 +1,61 @@
+<?php
+
+use model\repositorio\UsuarioRepositorio;
+
+require_once __DIR__ . "/../../../../model/repositorio/UsuarioRepositorio.php";
+require_once __DIR__ . "/../../../../model/usuario/Usuario.php";
+require_once __DIR__ . "/../../../../controller/conexao-bd.php";
+
+session_start();
+$usuario_logado = $_SESSION['usuario'] ?? null;
+
+if (!$usuario_logado) {
+    header('Location: /SistemaShopping_web1/src/view/sessoes/login.php?erro=deslogado');
+    exit;
+}
+
+$repositorio = new UsuarioRepositorio($pdo);
+$usuario = (new UsuarioRepositorio($pdo))->buscarPorEmail($usuario_logado);
+
+$erro = $_GET['erro'] ?? null;
+
+$cargo = $usuario->getCargo();
+if($cargo == Cargo::Funcionario_cinema || $cargo == Cargo::Gerenciador_anuncio){
+    header('Location: /SistemaShopping_web1/src/view/administrativo/administrativo.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Categoria de Loja - SchweizerPine Shopping</title>
+    <link rel="icon" type="image/png" href="/SistemaShopping_web1/img/logoShopping.png">
+    <link rel="stylesheet" href="/SistemaShopping_web1/css/cadastrar.css">
+</head>
+
+<body>
+<?php include('../../menu.php') ?>
+<main>
+    <section class="cadastro-container">
+        <div class="form-box">
+            <?php if (isset($_GET['erro']) && $_GET['erro'] === 'campos-vazios'): ?>
+                <p class="mensagem-erro">Não deixe os campos vazios.</p>
+            <?php endif; ?>
+            <h2>Cadastro de Categoria (Loja)</h2>
+            <form action="/SistemaShopping_web1/src/controller/cadastro/registrar_categoria_loja.php" method="post" enctype="multipart/form-data">
+                <label for="nomeCategoria">Categoria</label>
+                <input type="text" id="nomeCategoria" name="categoria" placeholder="Digite o nome da categoria" value="<?= $_GET['categoria'] ?>" required>
+                <input type="hidden" id="id" name="id" value="<?= $_GET['id'] ?>"/>
+                <input class="btn-cadastrar" type="submit" value="Cadastrar"/>
+            </form>
+        </div>
+    </section>
+</main>
+
+</body>
+
+</html>
